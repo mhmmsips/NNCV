158e138bcaca6ca8c1e7405983afd04da8014137
mhmmsips <m.h.m.m.sips@student.tue.nl>
2026-03-09 16:20:09 +0100

    First attempt DINOv2

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,8 +3,8 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 10 \
+    --epochs 25 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet-training" \
+    --experiment-id "DINOv2-training" \
