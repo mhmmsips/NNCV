a9c2d1a256164749869373cfd82347c34649e799
MHMMSips <m.h.m.m.sips@student.tue.nl>
2026-03-23 13:50:34 +0100

    Cleaned up the repo

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -9,12 +9,12 @@ CUDA_LAUNCH_BLOCKING=1 python3 -m accelerate.commands.launch \
     --num_processes ${num_processes} \
     train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 4 \
+    --batch-size 8 \
     --epochs 50 \
-    --lr 0.01 \
+    --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --gradient-accumulation-steps 2 \
+    --gradient-accumulation-steps 1 \
     --mixed-precision "fp16" \
-    --experiment-id "DINOv2_upsample_old" \
-    --decoder "upsample"
+    --experiment-id "ADAMW_UNet_CED"
+    # --decoder "upsample"
